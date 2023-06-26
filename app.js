b963e9b8b623660b4ea01698576e2c0ca6d2888f
Adolphus Okoro <adolphusokoro@Adolphuss-MacBook-Air.local>
2023-06-26 16:16:54 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+
+async function run(){
+    const s3Client = new S3Client({
+        region: 'us-east-1'
+    })
+    const command = new GetObjectCommand({
+        Bucket: 'qubit-app1-bucket',
+        Key: 'my_script.txt'
+    })
+    const response = await s3Client.send(command)
+    console.log(response)
+
+}
